fc14f62fab4945f6659c1327e48af8f98f2127c1
fathan <fathan.a.dev@gmail.com>
2022-03-27 23:11:01 +0700

    CallBack Hell Prevention

--- app/models/auth_db.js
+++ app/models/auth_db.js
@@ -4,62 +4,88 @@ const res = require('express/lib/response')
 const { MongoClient } = require("mongodb");
 const { failedWithMessageResponse, successWithMessageResponse } = require('../views/json_responses/response');
 
-const mongoUri = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.3.1'
+const mongoUri = 'mongodb://127.0.0.1:27017/'
 
 class User{
-    constructor(username, password, email, nama,kelas) {
+    constructor(username, password, email, nama) {
 		this.username = username
 		this.password = password
         this.email = email
         this.nama = nama
-        this.kelas = kelas
 	}
     jsonObject() {
         return {
             username: this.username, 
             password: this.password, 
             email: this.email, 
-            nama:this.nama,
-            kelas:this.kelas
+            nama:this.nama
         }
     }
 }
-
+const getDBCollection = (err, db, res) => {
+    if (err) {
+        res.send(failedWithMessageResponse(400,"something went wrong"))
+        
+        return Promise.resolve("something went wrong")
+        
+    }else return Promise.resolve(null)
+}
 
 module.exports = {
     registerDB: async (data,res)=>{
         try {
-            const user = new User(data.username, data.password, data.email, data.nama,  data.kelas)
-            MongoClient.connect(mongoUri, async function(err, db) {
-                if (err) res.send(failedWithMessageResponse(400,err))
+            const user = new User(data.username, data.password, data.email, data.nama)
+            MongoClient.connect(mongoUri, (err,db)=>{
+                
+                
                 var dbo = db.db("testing");
-                const dbUsers = dbo.collection("users")
-                dbUsers.findOne({username: user.jsonObject().username}, function(err,result) {
-                    if (!result) {
-                        dbUsers.findOne({email: user.jsonObject().email}, function(err,result) {
+                var dbUsers = dbo.collection("users")
+                getDBCollection(err,db,res)
+                    .then(async (err)=>{
+                        if (!err) {
+                            return await dbUsers.findOne({username: user.jsonObject().username}), null
+                                
+                        }else return null,"DB Error"
+                        
+                    })
+                    .then(async (result,err)=>{
+                        
+                        if (!err) {
                             if (!result) {
-                                dbUsers.insertOne(user.jsonObject(), function(err, result) {
-                                    if (err) res.send(failedWithMessageResponse(400,err))
-                                    else {
-                                        res.send(successWithMessageResponse("successfully registered an account"))
-                                        db.close()
-                                    }
-                                })
+                                return await dbUsers.findOne({email: user.jsonObject().email})
                             }else{
-                                res.send(failedWithMessageResponse(400,"email already exist."))
+                                res.send(failedWithMessageResponse(400,"username already exist."))
                                 db.close()
                             }
-                        })
-                    }else{
-                        res.send(failedWithMessageResponse(400,"username already exist."))
+                        }else{
+                            res.send(failedWithMessageResponse(400,"something went wrong"))
+                            db.close()
+                        }
+                    })
+                    .then(async (result)=>{
+                        
+                        if (!result) {
+                            await dbUsers.insertOne(user.jsonObject())
+                            return null
+                                
+                        }else{
+                            db.close()
+                            return "email already exist."
+                        }
+                    })
+                    .then((err)=>{
+                                    
+                        if (err) {
+                            res.send(failedWithMessageResponse(400,err))
+                            
+                        }
+                        else {
+                            res.send(successWithMessageResponse("successfully registered an account"))
+                        }
                         db.close()
-                    }
-                    
-                })
-
+                    }) 
                 
-              })
-            
+            })
         } catch (error) {
             console.log(error)
         }
